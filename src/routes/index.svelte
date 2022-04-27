20b8dbae68c0437f041899238eb31f9183e6a3f2
Joseph Bravo <josephangelobravo@gmail.com>
2022-04-27 03:14:08 +0000

    starting work on animations

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,6 @@
 <div class="h-screen grid justify-center items-center bg-rose-200">
 
-  <div class="bg-gray-50 rounded-full px-12 py-4 text-center shadow-md">
+  <div class="banner bg-gray-50 rounded-full px-12 py-4 text-center shadow-md">
     <h1 class="text-3xl font-bold">Svelte + TailwindCSS</h1>
     <h2>just another template | <a href="https://github.com/joseph-bravo/svelte-tailwind">github</a></h2>
   </div>
@@ -11,4 +11,18 @@
   a {
     @apply text-red-900 hover:brightness-125;
   }
+
+  .banner::before {
+    animation: pulse 0.5s;
+  }
+
+  @keyframes pulse {
+    0% {
+      @apply bg-black;
+    }
+    100% {
+      @apply bg-white;
+    }
+  }
+
 </style>
